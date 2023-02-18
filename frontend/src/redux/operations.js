79199963ff92151c7b9219412126ffd7057b0e1e
Marina Sereda <marina.gorb@gmail.com>
2023-02-18 23:57:03 +0200

    feat: add env

--- frontend/src/redux/operations.js
+++ frontend/src/redux/operations.js
@@ -3,11 +3,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const sendFeedBack = createAsyncThunk(
   "feedback/send",
   async (feedback, thunkApi) => {
-    const response = await fetch(import.meta.env.VITE_SERVER_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(feedback),
-    });
+    const response = await fetch(
+      `${import.meta.env.VITE_SERVER_URL}/feedbacks`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(feedback),
+      }
+    );
 
     const body = await response.json();
     if (!response.ok) {
