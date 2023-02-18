66c18011d9e78f1f3af23bc151642c5a80712f3a
Marina Sereda <marina.gorb@gmail.com>
2023-02-19 00:16:31 +0200

    fix: images path

--- frontend/src/components/Feedback/Feedback.styled.js
+++ frontend/src/components/Feedback/Feedback.styled.js
@@ -2,38 +2,30 @@ import styled from "styled-components";
 
 export const Section = styled.section`
   padding: 180px 0 174px;
-  background: url(../../src/assets/images/yellow-smile-1.svg) no-repeat top 3%
-      left 3%,
-    url(../../src/assets/images/yellow-smile-2.svg) no-repeat top 65% right 42%,
-    url(../../src/assets/images/pink-smile.svg) no-repeat top 88% right 33%,
-    url(../../src/assets/images/cloud-5.svg) no-repeat top 25% left -5% / 246px,
-    url(../../src/assets/images/cloud-15.svg) no-repeat top 52% left -7% / 218px,
-    url(../../src/assets/images/cloud-10.svg) no-repeat top 83% left 7% / 132px,
-    url(../../src/assets/images/cloud-15.svg) no-repeat top 82% left 28% / 218px,
-    url(../../src/assets/images/cloud-5.svg) no-repeat top 107% left 13% / 288px,
-    url(../../src/assets/images/cloud-5.svg) no-repeat top 16% right 51% / 138px,
-    url(../../src/assets/images/cloud-15.svg) no-repeat top 13% right 35% /
-      218px,
-    url(../../src/assets/images/cloud-10.svg) no-repeat top 31% right 45% /
-      172px,
-    url(../../src/assets/images/cloud-10.svg) no-repeat top 93% right 43% /
-      170px,
-    url(../../src/assets/images/circle.svg) no-repeat top 2% right 46%,
-    url(../../src/assets/images/map.jpg) no-repeat top 0% right 0%;
+  background: url(images/yellow-smile-1.svg) no-repeat top 3% left 3%,
+    url(images/yellow-smile-2.svg) no-repeat top 65% right 42%,
+    url(images/pink-smile.svg) no-repeat top 88% right 33%,
+    url(images/cloud-5.svg) no-repeat top 25% left -5% / 246px,
+    url(images/cloud-15.svg) no-repeat top 52% left -7% / 218px,
+    url(images/cloud-10.svg) no-repeat top 83% left 7% / 132px,
+    url(images/cloud-15.svg) no-repeat top 82% left 28% / 218px,
+    url(images/cloud-5.svg) no-repeat top 107% left 13% / 288px,
+    url(images/cloud-5.svg) no-repeat top 16% right 51% / 138px,
+    url(images/cloud-15.svg) no-repeat top 13% right 35% / 218px,
+    url(images/cloud-10.svg) no-repeat top 31% right 45% / 172px,
+    url(images/cloud-10.svg) no-repeat top 93% right 43% / 170px,
+    url(images/circle.svg) no-repeat top 2% right 46%,
+    url(images/map.jpg) no-repeat top 0% right 0%;
 
   @media (max-width: 1024px) {
     padding: 120px 0;
-    background: url(../../src/assets/images/yellow-smile-1.svg) no-repeat top 3%
-        left 3%,
-      url(../../src/assets/images/cloud-5.svg) no-repeat top 25% left -5% / 246px,
-      url(../../src/assets/images/cloud-15.svg) no-repeat top 52% left -7% / 218px,
-      url(../../src/assets/images/cloud-10.svg) no-repeat top 83% left 7% /
-        132px,
-      url(../../src/assets/images/cloud-15.svg) no-repeat top 82% left 28% /
-        218px,
-      url(../../src/assets/images/cloud-5.svg) no-repeat top 107% left 13% /
-        288px,
-      url(../../src/assets/images/map.jpg) no-repeat top 0% right 0%;
+    background: url(images/yellow-smile-1.svg) no-repeat top 3% left 3%,
+      url(images/cloud-5.svg) no-repeat top 25% left -5% / 246px,
+      url(images/cloud-15.svg) no-repeat top 52% left -7% / 218px,
+      url(images/cloud-10.svg) no-repeat top 83% left 7% / 132px,
+      url(images/cloud-15.svg) no-repeat top 82% left 28% / 218px,
+      url(images/cloud-5.svg) no-repeat top 107% left 13% / 288px,
+      url(images/map.jpg) no-repeat top 0% right 0%;
   }
 
   @media (max-width: 640px) {
--- frontend/src/components/Footer/Footer.styled.js
+++ frontend/src/components/Footer/Footer.styled.js
@@ -2,27 +2,21 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   padding: 72px 0 96px;
-  background: url("../../src/assets/images/pink-smile.svg") no-repeat top 40%
-      left 2%,
-    url("../../src/assets/images/green-smile.svg") no-repeat top 0% right 10%,
-    url("../../src/assets/images/yellow-smile-1.svg") no-repeat top 70% right -3%,
-    #fafafa;
+  background: url(images/pink-smile.svg) no-repeat top 40% left 2%,
+    url(images/green-smile.svg) no-repeat top 0% right 10%,
+    url(images/yellow-smile-1.svg) no-repeat top 70% right -3%, #fafafa;
   border-top: 1px solid #d8d8d8;
 
   @media (max-width: 1024px) {
     padding: 60px 0;
-    background: url("../../src/assets/images/pink-smile.svg") no-repeat top 15%
-        left -10%,
-      url("../../src/assets/images/green-smile.svg") no-repeat top 0% right 10%,
-      #fafafa;
+    background: url(images/pink-smile.svg) no-repeat top 15% left -10%,
+      url(images/green-smile.svg) no-repeat top 0% right 10%, #fafafa;
   }
 
   @media (max-width: 640px) {
     padding: 40px 0;
-    background: url("../../src/assets/images/pink-smile.svg") no-repeat top 15%
-        left -10% /150px,
-      url("../../src/assets/images/green-smile.svg") no-repeat top 0% right 10%,
-      #fafafa;
+    background: url(images/pink-smile.svg) no-repeat top 15% left -10% /150px,
+      url(images/green-smile.svg) no-repeat top 0% right 10%, #fafafa;
   }
 `;
 
